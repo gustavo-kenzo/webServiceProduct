96099843cd505b4d4b4a3595f832a189b3da13a8
Gustavo <sgkenzo212007@gmail.com>
2025-10-08 11:16:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/gustavo/webservice/services/UserService.java
@@ -0,0 +1,24 @@
+package com.gustavo.webservice.services;
+
+import com.gustavo.webservice.entities.User;
+import com.gustavo.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
